09678c4163152ea223727189cb57c62dd64155c4
Alexander Hermans <hermans@vision.rwth-aachen.de>
2015-11-06 14:07:59 +0100

    Fixed a problem that caused the label image borders to be void during downscaling.

--- utils.py
+++ utils.py
@@ -14,10 +14,15 @@ def downscale_labels(factor, labels, threshold):
     for y in xrange(0, h):
         for x in xrange(0, w):
             label_sums[y/factor, x/factor, labels[y,x]] +=1
+
+    hit_counts = np.sum(label_sums,2)
+
     label_sums = label_sums[:,:,:-1]
     new_labels = np.argsort(label_sums, 2)[:,:,-1].astype(np.int32)
     counts = label_sums.reshape(h_n*w_n, M+1)
     counts = counts[np.arange(h_n*w_n),new_labels.flat]
     counts = counts.reshape((h_n, w_n))
-    new_labels[counts < factor*factor*threshold] = -1
+
+    hit_counts *=threshold
+    new_labels[counts < hit_counts] = -1
     return new_labels
